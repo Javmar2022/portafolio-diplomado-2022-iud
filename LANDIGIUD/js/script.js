d6a80d3b856b31d7b92519c2c6d814fcc0cf2916
Javmar2022 <javmar04@yahoo.es>
2022-10-14 16:53:37 -0500

    :lipstick: Style & media queries

---
+++ LANDIGIUD/js/script.js
@@ -0,0 +1,42 @@
+const foot = document.getElementById('year');
+
+const year = new Date().getFullYear();
+
+foot.innerHTML = year;
+  
+//menu
+
+const buttonMenu = document.getElementById('btn-menu')
+const list = document.getElementById('nav-items')
+let show = true;
+
+buttonMenu.addEventListener('click', ()=>{
+    show = !show;
+    if(show){
+        list.style.display = 'block'
+    }else{
+        list.style.display = 'none'
+    }
+})
+
+window.addEventListener('resize', () => {
+    const width = screen.width
+    if(width >=425.999){
+        list.style.display = 'flex'
+
+    }else{
+        list.style.display = 'none'
+        show = false
+    }
+})
+
+(function(){
+    const width = screen.width
+    if(width >=425.999){
+        list.style.display = 'flex'
+
+    }else{
+        list.style.display = 'none'
+        show = false
+    }
+})();
